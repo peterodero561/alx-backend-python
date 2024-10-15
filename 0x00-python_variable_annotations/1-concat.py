2731a38f24971208629f5f5ab021ead2026c23ba
Peter Odero <peterodero561@gmail.com>
2024-10-15 14:35:26 +0300

    fix: correct datatype of string

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,6 +2,6 @@
 '''Fucrtion to concatenate two strings'''
 
 
-def concat(str1: string, str2: string) -> string:
+def concat(str1: str, str2: str) -> str:
     '''returns concatenation of str1 and str2'''
     return str1 + str2
