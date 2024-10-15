32eec7109ee6d0103e8bb6313ba6e1b02ab9f7e5
Peter Odero <peterodero561@gmail.com>
2024-10-15 14:29:39 +0300

    build: create add() to add two floats

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''Function to add to float variables'''
+
+
+def add(a: float, b: float) -> float:
+    '''returns the sum of the given vrariables a and b'''
+    return a+b
