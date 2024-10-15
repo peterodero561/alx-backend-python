5d7dcb5b70c149d80a69ea44d10c8ab6b9462ff6
Peter Odero <peterodero561@gmail.com>
2024-10-15 15:48:42 +0300

    corrected

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -3,6 +3,6 @@
 from typing import Tuple, Iterable, Sequence, List
 
 
-def element_length(lst: Iterable[Sequence])-> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     '''returns the elements of a list with appropiate values'''
     return [(i, len(i)) for i in lst]
