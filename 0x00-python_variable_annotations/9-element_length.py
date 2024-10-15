991829e1bf08aa4765448717dcac4d4489a097ca
Peter Odero <peterodero561@gmail.com>
2024-10-15 15:43:03 +0300

    corrected import

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''Annotate to and return values with the appropriate types'''
-from typing import Union, List, Any
+from typing import Tuple, List, Any
 
 
 def element_length(lst: List[Any]) -> List[Tuple[Any, int]]:
