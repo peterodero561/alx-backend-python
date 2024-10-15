37bf0b0a88faea5a54205063d4271fe6aadd4684
Peter Odero <peterodero561@gmail.com>
2024-10-15 15:12:22 +0300

    fix: type of annotated function

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,6 +2,6 @@
 '''Function to return a tuple'''
 from typing import Tuple, Union
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''returns a tuple of the given parameters'''
     return (k, v)
