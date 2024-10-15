b5546a74af9778f96046954742ee6c932a89ca21
Peter Odero <peterodero561@gmail.com>
2024-10-15 15:10:48 +0300

    fix

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''Function to return a tuple'''
+from typing import Tuple, Union
 
-
-def to_kv(k: str, v: int/float) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
     '''returns a tuple of the given parameters'''
     return (k, v)
