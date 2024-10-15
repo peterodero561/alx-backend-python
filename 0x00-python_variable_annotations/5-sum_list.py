f1acfc532f99b37a424cb336864637b0309f1f24
Peter Odero <peterodero561@gmail.com>
2024-10-15 14:53:55 +0300

    build: function to add elementas of a list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Function to sum up a list'''
+from typing import LIST
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''returs sum of the elements of a list'''
+    return sum(input_list)
