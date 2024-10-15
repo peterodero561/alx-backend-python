0ba88eaecfec72f8733545cc913079ce942953a6
Peter Odero <peterodero561@gmail.com>
2024-10-15 14:33:35 +0300

    build: function concat() that concatenates two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''Fucrtion to concatenate two strings'''
+
+
+def concat(str1: string, str2: string) -> string:
+    '''returns concatenation of str1 and str2'''
+    return str1 + str2
