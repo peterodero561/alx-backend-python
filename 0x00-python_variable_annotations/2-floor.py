a40986afa0db3228cc110d31dbd78fbeae729bf5
Peter Odero <peterodero561@gmail.com>
2024-10-15 14:41:21 +0300

    build: created floor() that returns floor of a given float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Function to return floor when given a float'''
+import math
+
+
+def floor(n: float) -> int:
+    '''returns the floor format of n'''
+    return math.floor(n)
