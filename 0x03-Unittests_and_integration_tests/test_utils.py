90d557658774622deae80449c984bcc660b557b9
Peter Odero <peterodero561@gmail.com>
2024-11-08 21:38:25 +0300

    fix: pycodestyle errors

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -43,18 +43,16 @@ class TestGetJson(unittest.TestCase):
             ("http://example.com", {"payload": True}),
             ("http://holberton.io", {"payload": False}),
         ]
-        
         for test_url, test_payload in test_cases:
             mock_response = Mock()
             mock_response.json.return_value = test_payload
             mock_get.return_value = mock_response
-            
             # Call get_json and check results
             result = get_json(test_url)
             self.assertEqual(result, test_payload)
-            
+
             # Verify that requests.get was called once with the test_url
             mock_get.assert_called_once_with(test_url)
-            
+
             # Reset the mock for the next iteration
             mock_get.reset_mock()
